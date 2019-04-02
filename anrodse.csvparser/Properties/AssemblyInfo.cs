3c9f7910c0c0daadb86aa5e886d5b2b1af52d049
anrodse <n4rco_87@hotmail.com>
2019-04-02 19:10:00 +0200

    - Creado fichero AssemblyInfo

--- anrodse.csvparser/Properties/AssemblyInfo.cs
+++ anrodse.csvparser/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("anrodse.csvparser")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CSV file parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("anrodse.csvparser")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCompany("anrodse")]
+[assembly: AssemblyProduct("CSV Parser")]
+[assembly: AssemblyCopyright("anrodse -ñ- 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
 // mediante el carácter "*", como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
